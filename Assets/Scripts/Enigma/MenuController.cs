6abdb02651d6b71cc314a18839257bf6b2ffb3e8
Guy Oded <umm_no@gmail.com>
2024-10-10 22:55:44 +0300

    Add a bunch of assets for the menu

---
+++ Assets/Scripts/Enigma/MenuController.cs
@@ -0,0 +1,31 @@
+using DG.Tweening;
+using UnityEngine;
+
+namespace Enigma
+{
+    public class MenuController : MonoBehaviour
+    {
+        [SerializeField] private GameObject _instructionsPanel;
+        [SerializeField] private RectTransform _instructionFrame;
+        [SerializeField] private EnigmaController _enigmaController;
+        [SerializeField] private bool _showInstructionsOnStart = true;
+
+        private void Start()
+        {
+            if (!_showInstructionsOnStart)
+            {
+                HideInstructions();
+                return;
+            }
+
+            _instructionFrame.localScale = Vector3.zero;
+            _instructionFrame.DOScale(Vector3.one, 0.4f).SetEase(Ease.OutBounce);
+        }
+
+        public void HideInstructions()
+        {
+            _instructionsPanel.SetActive(false);
+            _enigmaController.AttachKeysInput();
+        }
+    }
+}
