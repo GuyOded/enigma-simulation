7998df547c338f6b1a9d1bfde7e0efeacb5bb88e
Guy Oded <guy@somemail.com>
2025-03-18 11:32:27 +0200

    Add scripts for controlling plugs outline

---
+++ Assets/Scripts/Enigma/LetterPlug.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Enigma
+{
+    public class LetterPlug : MonoBehaviour
+    {
+        [SerializeField] private Outline _outline;
+
+        public Outline Outline
+        {
+            get;
+            private set;
+        }
+
+        private void Start()
+        {
+            Outline = _outline;
+        }
+    }
+}
---
+++ Assets/Scripts/Enigma/PlugboardController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using AYellowpaper.SerializedCollections;
+using UnityEngine;
+
+namespace Enigma
+{
+    public class PlugboardController : MonoBehaviour
+    {
+        [SerializedDictionary("Letter", "Game Object")] [SerializeField]
+        private SerializedDictionary<char, LetterPlug> _letterPlugsMap = new()
+        {
+            { 'A', null },
+            { 'B', null },
+            { 'C', null },
+            { 'D', null },
+            { 'E', null },
+            { 'F', null },
+            { 'G', null },
+            { 'H', null },
+            { 'I', null },
+            { 'J', null },
+            { 'K', null },
+            { 'L', null },
+            { 'M', null },
+            { 'N', null },
+            { 'O', null },
+            { 'P', null },
+            { 'Q', null },
+            { 'R', null },
+            { 'S', null },
+            { 'T', null },
+            { 'U', null },
+            { 'V', null },
+            { 'W', null },
+            { 'X', null },
+            { 'Y', null },
+            { 'Z', null }
+        };
+    }
+}
