59f27d3f7229c1e7fd07009606f293fbba83dc00
Guy Oded <guy@somemail.com>
2025-03-18 13:22:57 +0200

    Add raycast code to enable outline when plug is clicked

--- Assets/Scripts/Enigma/PlugboardController.cs
+++ Assets/Scripts/Enigma/PlugboardController.cs
@@ -35,6 +35,9 @@ namespace Enigma
             { 'Y', null },
             { 'Z', null }
         };
+        [SerializeField] private Camera _mainCamera;
+
+        private const float RAYCAST_LENGTH = 100f;
 
         private bool _isClickEventsActive;
 
@@ -63,7 +66,23 @@ namespace Enigma
 
         private void CastRayInMouseDirection()
         {
-            Debug.Log("This is active!");
+            Ray outgoingRay = _mainCamera.ScreenPointToRay(Input.mousePosition);
+            if (!Physics.Raycast(outgoingRay, out RaycastHit hit, RAYCAST_LENGTH, LayerMask.GetMask("LetterPlugs")))
+            {
+                foreach (LetterPlug letterPlug in _letterPlugsMap.Values)
+                    letterPlug.Outline.enabled = false;
+
+                return;
+            }
+
+            char letter = hit.collider.tag[0];
+            if (!_letterPlugsMap.TryGetValue(letter, out LetterPlug plug))
+            {
+                Debug.LogError($"Raycast hit but found no object in letter to object map. Tag hit is {hit.collider.tag}");
+                return;
+            }
+
+            plug.Outline.enabled = true;
         }
     }
 }
